7fadb872d3da44dad0039baca8526d25cf3b1ce7
Davide Chiappa <davide.app99@gmail.com>
2018-04-06 11:53:54 +0200

    kia3bgèuiearpgb'8wsubverpiughbe08yh

--- app/src/main/java/it/itisplanck/davide/it/lingue/MainActivity.java
+++ app/src/main/java/it/itisplanck/davide/it/lingue/MainActivity.java
@@ -8,26 +8,47 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText e;
+    private EditText username,password,nome,cognome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e=findViewById(R.id.editText2);
+        username=findViewById(R.id.usernameEdit);
+        nome=findViewById(R.id.nomeEdit);
+        cognome=findViewById(R.id.cognomeEdit);
+        password=findViewById(R.id.passwordEdit);
+
     }
     public void registrazione(View v){
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setTitle(getString(R.string.DialogTitle)+e.getText().toString()+"!");
-        builder1.setMessage(R.string.DialogMessage);
-        builder1.setCancelable(true);
-        builder1.setNeutralButton(android.R.string.ok,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
+        if(username.getText().toString().equals("") || nome.getText().toString().equals("")
+                || cognome.getText().toString().equals("") || password.getText().toString().equals("")){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setTitle(getString(R.string.errorDialog));
+            builder1.setIcon(R.drawable.ic_warning_yellow_24dp);
+            builder1.setMessage(R.string.errorMessage);
+            builder1.setCancelable(true);
+            builder1.setNeutralButton(android.R.string.ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+        else{
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setTitle(getString(R.string.DialogTitle)+username.getText().toString()+"");
+            builder1.setMessage(R.string.DialogMessage);
+            builder1.setCancelable(true);
+            builder1.setNeutralButton(android.R.string.ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
     }
 }
